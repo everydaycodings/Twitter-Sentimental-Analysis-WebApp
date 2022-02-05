93513f4e52a4609a1d97c74f83219bf25ec703a3
Kumar Saksham <everydaycodings@gmail.com>
2022-02-05 19:04:22 +0530

    added preprocessing_data() in helper.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from helper import preprocessing_data
 
 st.title("Twitter Sentimental Analysis")
 
@@ -6,3 +7,8 @@ word_query = st.text_input("Enter the Hastag or any word")
 number_of_tweets = st.slider("How many tweets YOu want o collect from {}".format(word_query), min_value=100, max_value=10000)
 st.info("1 Tweets takes approx 0.1 sec so you may have to wait {} minute for {} Tweets, So Please Have Patient.".format(round((number_of_tweets*0.1/60),2), number_of_tweets))
 
+if st.button("hi"):
+
+    data = preprocessing_data(word_query, number_of_tweets)
+
+    st.write(data)
---
+++ helper.py
@@ -0,0 +1,89 @@
+import tweepy
+import pandas as pd
+import configparser
+import re
+from textblob import TextBlob
+from wordcloud import WordCloud
+
+emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+
+
+
+
+def twitter_connection():
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    api_key = config["twitter"]["api_key"]
+    api_key_secret = config["twitter"]["api_key_secret"]
+    access_token = config["twitter"]["access_token"]
+
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    api = tweepy.API(auth)
+
+    return api
+
+api = twitter_connection()
+
+
+def cleanTxt(text):
+    text = re.sub('@[A-Za-z0–9]+', '', text) #Removing @mentions
+    text = re.sub('#', '', text) # Removing '#' hash tag
+    text = re.sub('RT[\s]+', '', text) # Removing RT
+    text = re.sub('https?:\/\/\S+', '', text)
+    text = re.sub("\n","",text) # Removing hyperlink
+    text = re.sub(":","",text) # Removing hyperlink
+    text = re.sub("_","",text) # Removing hyperlink
+    text = emoji_pattern.sub(r'', text)
+    return text
+
+def getSubjectivity(text):
+   return TextBlob(text).sentiment.subjectivity
+
+# Create a function to get the polarity
+def getPolarity(text):
+   return  TextBlob(text).sentiment.polarity
+
+def getAnalysis(score):
+  if score < 0:
+    return 'Negative'
+  elif score == 0:
+    return 'Neutral'
+  else:
+    return 'Positive'
+
+
+def preprocessing_data(word_query, number_of_tweets):
+    posts = tweepy.Cursor(api.search_tweets, q=word_query, count = 200, lang ="en", tweet_mode="extended").items((number_of_tweets))
+    data  = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
+
+    data['Tweets'] = data['Tweets'].apply(cleanTxt)
+    discard = ["CNFTGiveaway", "IVEAWAYPrizes", "Giveaway", "Airdrop"]
+    data = data[~data["Tweets"].str.contains('|'.join(discard))]
+
+    data['Subjectivity'] = data['Tweets'].apply(getSubjectivity)
+    data['Polarity'] = data['Tweets'].apply(getPolarity)
+
+    data['Analysis'] = data['Polarity'].apply(getAnalysis)
+
+    return data
