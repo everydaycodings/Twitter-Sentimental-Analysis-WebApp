7a3fd8b69ecdacb985b38d85c9a1586f904c2a36
Kumar Saksham <everydaycodings@gmail.com>
2022-02-05 21:03:33 +0530

    addded column for mentions links and retweets in sentimental_analysis.ipynb

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if function_option == "Search By #Tag and Words":
     word_query = st.text_input("Enter the Hastag or any word")
 
 if function_option == "Search By Username":
-    word_query = st.text_input("Enter the Username (Dont include @)")
+    word_query = st.text_input("Enter the Username ( Don't include @ )")
 
 number_of_tweets = st.slider("How many tweets You want to collect from {}".format(word_query), min_value=100, max_value=10000)
 st.info("1 Tweets takes approx 0.05 sec so you may have to wait {} minute for {} Tweets, So Please Have Patient.".format(round((number_of_tweets*0.05/60),2), number_of_tweets))
